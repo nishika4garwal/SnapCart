91e8a11eddc3a47b1f601be2064afd094d013ba2
nishika <nishika.agarwal0402@gmail.com>
2025-04-16 00:54:16 +0530

    .

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy # type: ignore
 from flask_migrate import Migrate # type: ignore
 from models import db, User, Product, Category, Cart, Order  # Import models
 import config
+import os
 
 app = Flask(__name__)
 
@@ -21,3 +22,7 @@ migrate = Migrate(app, db)
 #     db.create_all()
 
 import routes  # Import routes after app and db are initialized
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Render provides this
+    app.run(host='0.0.0.0', port=port)
